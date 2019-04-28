484317e0c59d35405ff0ed625e133e74d70b1abf
Ibrahima Sarr <desert_jellyfish@riseup.net>
2019-04-28 18:32:27 +0000

    rm node_modules

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var mongojs = require('mongojs');
-
+var http = require('http')
 
 const db = mongojs("mongodb://ab_ibrahima:FR2DwqIROWHmLlgX@cluster0-shard-00-02-zl8ja.mongodb.net:27017,cluster0-shard-00-00-zl8ja.mongodb.net:27017,cluster0-shard-00-01-zl8ja.mongodb.net:27017/sample_airbnb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true")
 
 var id = Math.floor(Math.random() * 100000);
 var collname = "test" + id;
+
+http.createServer(function (req, res) {
+    
+    if (req.method = 'GET') {
+	console.log(req.headers.referer);
+	res.write('Hello World!');
+	res.end();
+    }
+    else 
+    if (req.method = 'POST') {
+	console.log(req.headers.referer);
+	res.write('Hello World!');
+	res.end();
+    }
+ 
+}).listen(8080);
+
 
 function getNumberInfo (number) {
     let info = {}
@@ -15,13 +32,13 @@ function getNumberInfo (number) {
     info.square = number * number;
     return info;
 }
-
+/*
 db.createCollection(collname,{});
 
 console.log("collection "+collname+" created\n")
 
 for (let i =0; i<1000; i++) {
-    console.log("insert   " + i)
+    //console.log("insert   " + i)
     db.collection(collname).insert(getNumberInfo(i));
 }
 
@@ -33,3 +50,4 @@ db.collection(collname).findOne({}, (err,doc) => {
 })
 
 console.log("done")
+*/
